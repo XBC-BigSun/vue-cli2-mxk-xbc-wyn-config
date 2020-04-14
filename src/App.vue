adee48f15efd22197da9be30338bd1581e3007e1
xiaobaocheng <604558835@qq.com>
2020-04-14 11:48:07 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     </transition>
     <!-- 肖宝成添加了一行注释 -->
     <tabar>
-      <!-- 肖宝成又添加了一行注释 -->
+      <!-- 肖宝成又添加了一行注释11 -->
       <tabar-item
         path-name="home"
         :active-style="activeStyle"
