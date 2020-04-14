b4872fbfb515e12a7ea22dc89d22c4ea2270f5b3
xiaobaocheng <604558835@qq.com>
2020-04-14 11:46:26 +0800

    马兴凯又添加了一行注释

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     </transition>
     <!-- 肖宝成添加了一行注释 -->
     <tabar>
-      <!-- 肖宝成又添加了一行注释 -->
+      <!-- 马兴凯又添加了一行注释 -->
       <tabar-item
         path-name="home"
         :active-style="activeStyle"
